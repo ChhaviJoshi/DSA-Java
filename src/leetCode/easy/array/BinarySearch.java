35eb8ee9bd50a749451ad6856a08f38faadc84a3
Chhavi Joshi <chhavijoshi904@gmail.com>
2026-06-19 22:57:50 +0530

    binary search

---
+++ src/leetCode/easy/array/BinarySearch.java
@@ -0,0 +1,26 @@
+package leetCode.easy.array;
+
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {                     //<=!! not only <
+            int mid = left + (right - left)/ 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(search(nums, 6));
+    }
+}
