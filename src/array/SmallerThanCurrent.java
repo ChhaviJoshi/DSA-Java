06ef72f54ddfceb65b3529757518ecb54e24c004
Chhavi Joshi <chhavijoshi904@gmail.com>
2026-07-03 20:34:50 +0530

    array easy

---
+++ src/array/SmallerThanCurrent.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Arrays;
+
+public class SmallerThanCurrent {
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[i] > nums[j]) count++;
+            }
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {8,1,4,4,2};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+}
