74767981833e509abc3bf54bdd9d2d3cc5560c1e
Chhavi Joshi <chhavijoshi904@gmail.com>
2026-09-01 21:40:00 +0530

    insertion in linkedlist

---
+++ src/linkedlist/Main.java
@@ -0,0 +1,15 @@
+package linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLastWithoutTail(99);
+        list.insertLast(99);
+        list.insertAtIndex(100, 3);
+        list.display();
+    }
+}
